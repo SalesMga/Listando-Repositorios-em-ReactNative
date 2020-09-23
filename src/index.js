63cc4e433ecee76620c0af5911369cc340f65e71
SalesMga <lucassalessa3@gmail.com>
2020-09-22 22:32:39 -0300

    Ajetando o projeto, e Configurando ELSINT, Prettier, Reactotron

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import './config/ReactotronConfig';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'orange',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+});
+
+export default function App() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.welcome}>Welcome React</Text>
+            <Text style={styles.welcome}>teste 1</Text>
+        </View>
+    );
+}
